e0a89d29d65bdeec555ba0c9b17de66298d06b16
Janhvi Patil <janhvipatil716@gmail.com>
2025-03-06 21:46:33 +0530

    fix: scroll to AI features when raven AI button is clicked

--- src/components/Features/AIFeatures/AIFeatures.tsx
+++ src/components/Features/AIFeatures/AIFeatures.tsx
@@ -5,7 +5,7 @@ import VideoSwitcher from './VideoSwitcher';
 
 const AIFeatures: React.FC = () => {
     return (
-        <div className="flex flex-col justify-center md:pb-36 pb-16 max-w-6xl mx-auto px-8 md:px-0">
+        <div id="ai-features" className="flex flex-col justify-center md:pb-36 pb-16 max-w-6xl mx-auto px-8 md:px-0">
             <div className='flex flex-col gap-2 mb-12'>
                 <div className='flex items-center gap-2 font-semibold text-gray-800 text-md'><HiSparkles /><p>Raven AI</p></div>
                 <h2 className='text-4xl sm:text-4xl md:text-5xl font-calsans'>
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -7,9 +7,25 @@ import VideoWithPlayButton from './VideoWithPlayButton';
 const Hero: FC = () => {
     const chevronIcon = useMemo(() => <FiChevronRight color="gray" />, []);
 
+    const scrollToAISection = () => {
+        const aiSection = document.getElementById('ai-features');
+        if (aiSection) {
+            // Offest is 100 pixels above the section
+            const offset = 100;
+            const sectionTop = aiSection.getBoundingClientRect().top + window.scrollY;
+            // Scroll to the adjusted position
+            window.scrollTo({
+                top: sectionTop - offset,
+                behavior: 'smooth',
+            });
+        }
+    };
+
     return (
         <section className="flex flex-col justify-center items-center py-24 px-4 mt-20 text-center">
-            <div className="flex justify-center items-center gap-2 mb-4 pl-4 pr-2 py-1 bg-gradient-to-r from-gray-100 to-white border border-gray-200 rounded-full shadow-base text-gray-600 text-[12px] font-medium tracking-wide hover:cursor-pointer hover:outline-double hover:outline-blue-100">
+            <div
+                onClick={scrollToAISection}
+                className="flex justify-center items-center gap-2 mb-4 pl-4 pr-2 py-1 bg-gradient-to-r from-gray-100 to-white border border-gray-200 rounded-full shadow-base text-gray-600 text-[12px] font-medium tracking-wide hover:cursor-pointer hover:outline-double hover:outline-blue-100">
                 Introducing Raven AI ✨ {chevronIcon}
             </div>
             <h1 className="text-5xl sm:text-5xl md:text-7xl lg:text-[81px] mb-8 md:w-1/3 lg:w-2/5 font-calsans">
